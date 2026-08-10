e2db7914e713d8ab6d9be103a36fce512c92a658
conghungtran <tranconghung2023@gmail.com>
2026-08-11 06:43:24 +0700

    all file for running on window OS

--- main.c
+++ main.c
@@ -5,8 +5,6 @@
 
 #define GPIOA_IDR   (*(volatile unsigned int*)0x40010808)
 
-
-
 int main(void)
 {
     // Enable clock
@@ -14,14 +12,21 @@ int main(void)
 
     GPIOA_CRL &= ~(0xF << 0);
     GPIOA_CRL |=  (0x2 << 0);
+    //
+    GPIOA_CRL &= ~(0xF << 4);
+    GPIOA_CRL |=  (0x2 << 4);
 
     GPIOA_ODR |= (1 << 0);
+    GPIOA_ODR |= (1 << 1);
 
     while(1)
     {
         GPIOA_ODR ^= (1 << 0);
+
+        GPIOA_ODR ^= (1 << 1);
+
         for (int i = 0; i < 500000; i++) {
-            // Delay
+              // Delay
         }
     }
 }
